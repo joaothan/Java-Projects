4eeff0c82ccaf4b137ee68dd57af4d68aa1416e5
Jonathan Lima <118406958+joaothan@users.noreply.github.com>
2022-11-20 14:40:58 -0500

    Averages Project

--- Main.java
+++ Main.java
@@ -1,47 +1,25 @@
 package p1;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-public class Main 
-{
-public static void main(String[] args) 
-{
-Scanner sc = new Scanner(System.in);
-String names[] = new String[2];
-double latlon[][] = new double[2][2];
-System.out.print("Enter name of first city: ");
-names[0] = sc.nextLine();
-System.out.print("Enter name of second city: ");
-names[1] = sc.nextLine();
-		
-System.out.print("Enter latitude and longitude of first city separated by a space (example 28.8 81.2): ");
-latlon[0][0] = sc.nextDouble();
-latlon[0][1] = sc.nextDouble();
-System.out.print("Enter latitude and longitude of second city separated by a space (example 28.8 81.2): ");
-latlon[1][0] = sc.nextDouble();
-latlon[1][1] = sc.nextDouble();
-System.out.println("Distance between "+names[0]+" and "+names[1]+" is "+distance(latlon[0][0],latlon[0][1],latlon[1][0],latlon[1][1])+" miles.");
-System.out.println(distance(28.6, -81.4, 27.9, -82.5) + " Miles\n");
-}
-	
-private static double distance(double lat1, double lon1, double lat2, double lon2) 
-{
-if ((lat1 == lat2) && (lon1 == lon2)) 
-{
-return 0;
-} 
-else 
-{
-double theta = lon1 - lon2;
-double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
-	+ Math.cos(Math.toRadians(lat1)) *Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-dist = Math.acos(dist);
-dist = Math.toDegrees(dist);
-dist = dist * 60 * 1.1515;
-return (dist);
-		}
-	
-	}
+public class Main {
 
- }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] students = new String[] { "Paul", "Tabitha", "Elaine" };
+		int[][] grades = new int[][] {
+			{ 65, 100, 82, 97, 100, 75 },
+			{ 99, 50, 100, 89, 71, 95 },
+			{ 81, 85, 93, 99, 84, 85 }
+		};
+		
+		for (int i = 0; i < grades.length; i++) {
+			System.out.print("Student " + i + ": ");
+			int average = 0;
+		for (int j = 0; j < grades[i].length; j++) {
+				System.out.print(grades[i][j] + " ");
+				average += grades[i][j];
+		}
+		average /= grades[i].length;
+		System.out.println(". Average: " + average);
+	}
+}
+}
