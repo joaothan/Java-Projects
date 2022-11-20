09aa8b839cc95a50b316a65895a92191d63d430f
Jonathan Lima <118406958+joaothan@users.noreply.github.com>
2022-11-20 14:24:16 -0500

    Update Main.java
    Formatting

--- pack1/Main.java
+++ pack1/Main.java
@@ -9,50 +9,41 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
- 
-    //Start
-    public void start(Stage s)
-    {
-        //Title
-        s.setTitle("Pr6");
- 
-        //TextField
-        TextField b = new TextField("Enter Name");
-        b.setPrefColumnCount(7);
-        
-        //TilePane
-        TilePane r = new TilePane();
- 
-        //Label
-        Label l = new Label("Your Name");
- 
-        //ActionEvent
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e)
-            {
-                l.setText(b.getText());
-            }
-        };
- 
-        //EnterKeyAction
-        b.setOnAction(event);
- 
-        //Adding TextField
-        r.getChildren().add(b);
-        r.getChildren().add(l);
- 
-        //Scene
-        Scene sc = new Scene(r, 200, 200);
-     
-        s.setScene(sc);
- 
-        s.show();
-    }
- 
-    public static void main(String args[])
-    {
-        //Launch
-        launch(args);
-    }
+ public class Main extends Application 
+{
+//Start
+ public void start(Stage s)
+{
+//Title
+ s.setTitle("Pr6"); 
+//TextField
+ TextField b = new TextField("Enter Name");
+ b.setPrefColumnCount(7);       
+//TilePane
+ TilePane r = new TilePane();
+//Label
+ Label l = new Label("Your Name"); 
+ //ActionEvent
+ EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() 
+{
+ public void handle(ActionEvent e)
+{
+ l.setText(b.getText());
+}
+};
+//Enter Key ActionEvent
+ b.setOnAction(event);
+//Adding TextField
+ r.getChildren().add(b);
+ r.getChildren().add(l);
+//Scene
+ Scene sc = new Scene(r, 200, 200);
+ s.setScene(sc);
+ s.show();
+} 
+ public static void main(String args[])
+{
+//Launch
+ launch(args);
+}
 }
